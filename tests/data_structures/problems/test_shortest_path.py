4b8732cfcfb6c406e18633507d1a34879230d272
KuSi833 <kabo.milicic@gmail.com>
2022-04-08 20:07:52 +0100

    shortest path test

---
+++ tests/data_structures/problems/test_shortest_path.py
@@ -0,0 +1,31 @@
+from py_ads.ds.graphs.undirected_graph import (
+    UndirectedGraph,
+    ShortestPathBFS,
+)
+
+
+def test_shortest_path_bfs():
+
+    graph = UndirectedGraph()
+    for node in ["Hamburg", "Frankfurt", "Berlin", "Magdeburg", "Chemitz", "Bonn", "Karlsruhe", "Munich"]:
+        graph.add_node(node)
+
+    for a, b in [
+        ("Hamburg", "Frankfurt"),
+        ("Hamburg", "Berlin"),
+        ("Frankfurt", "Magdeburg"),
+        ("Berlin", "Bonn"),
+        ("Berlin", "Magdeburg"),
+        ("Magdeburg", "Chemitz"),
+        ("Bonn", "Chemitz"),
+        ("Bonn", "Karlsruhe"),
+        ("Bonn", "Munich"),
+        ("Chemitz", "Munich"),
+    ]:
+        graph.add_edge(a, b)
+
+    graph.remove_node("Bonn")
+    graph.remove_edge("Berlin", "Magdeburg")
+
+    assert (ShortestPathBFS(graph, "Berlin", "Munich") == "Berlin->Hamburg->Frankfurt->Magdeburg->Chemitz->Munich")
+    assert (ShortestPathBFS(graph, "Hamburg", "Karlsruhe") == "-1")
