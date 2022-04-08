e9cca366d0d573cec4509fb7b7cd6b6ca546a8a9
KuSi833 <kabo.milicic@gmail.com>
2022-04-08 20:08:37 +0100

    undirected graph

---
+++ py_ads/ds/graphs/undirected_graph.py
@@ -0,0 +1,31 @@
+# Data Structure
+
+
+class UndirectedGraph:
+    def __init__(self):
+        self.map = dict()
+
+    def adjacent(self, nodeA: str, nodeB: str):
+        return nodeA in self.map[nodeB] and nodeB in self.map[nodeA]
+
+    def add_node(self, node: str):
+        self.map[node] = set()
+
+    def remove_node(self, node: str):
+        for edges in self.map.values():
+            edges.discard(node)
+        self.map.pop(node)
+
+    def add_edge(self, nodeA: str, nodeB: str):
+        self.map[nodeA].add(nodeB)
+        self.map[nodeB].add(nodeA)
+
+    def remove_edge(self, nodeA: str, nodeB: str):
+        self.map[nodeA].remove(nodeB)
+        self.map[nodeB].remove(nodeA)
+
+    def get_neighbors(self, node: str):
+        return self.map[node]
+
+
+# Algorithms
