db52b5498319014f3299f27675e0757890eb6ac3
KuSi833 <kabo.milicic@gmail.com>
2022-04-09 20:34:19 +0100

    Value type and Biary Tree Size

--- py_ads/ds/binary_tree.py
+++ py_ads/ds/binary_tree.py
@@ -1,5 +1,9 @@
+from typing import List
+from py_ads.types import Value
+
+
 class BinaryTreeNode:
-    def __init__(self, val, left=None, right=None) -> None:
+    def __init__(self, val: Value, left=None, right=None) -> None:
         self.val = val
         self.left = left
         self.right = right
@@ -10,3 +14,9 @@ def find(node: BinaryTreeNode, val):
         if node.val == val:
             return node
         return find(node.left, val) or find(node.right, val)
+
+
+def size(node: BinaryTreeNode):
+    if node:
+        return 1 + size(node.left) + size(node.right)
+    return 0
---
+++ py_ads/types.py
@@ -0,0 +1,4 @@
+from typing import Union
+from typing_extensions import TypeAlias
+
+Value: TypeAlias = Union(str, int, float, complex, bool)
