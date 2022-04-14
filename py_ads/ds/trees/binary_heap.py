948e0d84b5c612bea485ffbaef94e62c9688a50f
KuSi833 <kabo.milicic@gmail.com>
2022-04-14 16:02:35 +0200

    Heap rework

--- py_ads/ds/trees/binary_heap.py
+++ py_ads/ds/trees/binary_heap.py
@@ -2,37 +2,87 @@ from typing import List, Any
 from abc import ABC, abstractmethod
 
 
-class HeapADT(ABC):
-    @abstractmethod
-    def root(self) -> Any:
-        pass
-
+class MaxHeapADT(ABC):
     @abstractmethod
     def __len__(self) -> int:
         pass
 
     @abstractmethod
-    def pop(self) -> Any:
+    def get_max(self) -> Any:
+        "Returns the maximum element"
         pass
 
     @abstractmethod
-    def push(self, val: Any):
+    def delete_max(self) -> None:
+        "Deletes the maximum element"
+
+    @abstractmethod
+    def extract_max(self) -> Any:
+        "Removes and returns the maximum element."
         pass
 
     @abstractmethod
-    def pushpop(self, val: Any):
+    def insert(self, element: Any) -> None:
+        "Insert a new element."
         pass
 
-
-class MaxHeapADT(HeapADT):
     @abstractmethod
-    def increase_key(self, i: int, increment: Any) -> None:
+    def extract_max_and_insert(self, element: Any) -> None:
+        "Extracts the maximum element and then inserts while sorting only once."
+        pass
+
+    # Knowledge of internal representaiton
+
+    @abstractmethod
+    def increase_key(self, i: int, decrement: Any) -> None:
+        "Increase the value of an element."
+        pass
+
+    @abstractmethod
+    def delete(self, i: int) -> None:
+        "Deletes an element."
         pass
 
 
-class MinHeapADT(HeapADT):
+class MinHeapADT(ABC):
+    @abstractmethod
+    def __len__(self) -> int:
+        pass
+
+    @abstractmethod
+    def get_min(self) -> Any:
+        "Returns the minimum element"
+        pass
+
+    @abstractmethod
+    def delete_min(self) -> None:
+        "Deletes the minimum element"
+
+    @abstractmethod
+    def extract_min(self) -> Any:
+        "Removes and returns the minimum element."
+        pass
+
+    @abstractmethod
+    def insert(self, val: Any) -> None:
+        "Insert a new element."
+        pass
+
+    @abstractmethod
+    def extract_min_and_insert(self, element: Any) -> None:
+        "Extracts the minimum element and then inserts while sorting only once."
+        pass
+
+    # Knowledge of internal representaiton
+
     @abstractmethod
     def decrease_key(self, i: int, decrement: Any) -> None:
+        "Decrease the value of a key."
+        pass
+
+    @abstractmethod
+    def delete(self, i: int) -> None:
+        "Deletes an element."
         pass
 
 
@@ -41,31 +91,44 @@ class MaxHeap(MaxHeapADT):
         self.heap: List[Any] = initial
         self._heapify()
 
-    def root(self) -> Any:
+    def get_max(self) -> Any:
         return self.heap[0]
 
-    def pop(self) -> Any:
-        root = self.heap[0]
-        # replace root with last element
+    def delete_max(self) -> None:
         self.heap[0] = self.heap.pop()
         self._sift_down(0)
+
+    def extract_max(self) -> Any:
+        root = self.heap[0]
+        # replace root with last element
+        if len(self) >= 2:
+            self.heap[0] = self.heap.pop()
+            self._sift_down(0)
+        else:
+            self.heap.pop()
         return root
 
-    def increase_key(self, i: int, increment: Any) -> None:
-        self.heap[i] += increment
-        self._sift_up(i)
-
-    def push(self, val: Any):
+    def insert(self, val: Any):
         self.heap.append(val)
         self._sift_up(len(self) - 1)
 
-    def pushpop(self, val: Any):
+    def extract_max_and_insert(self, val: Any):
         if val > self.root():
             return val
         root = self.root()
         self.heap[0] = val
         self._sift_down(0)
         return root
+
+    # Knowledge of internal representation
+
+    def increase_key(self, i: int, increment: Any) -> None:
+        self.heap[i] += increment
+        self._sift_up(i)
+
+    def delete(self, i: int) -> None:
+        self.increase_key(i, float('inf'))
+        self.delete_max()
 
     # Internal
 
@@ -111,20 +174,29 @@ class MinHeap(MinHeapADT):
     def __init__(self, initial: List[Any] = []) -> None:
         self.heap = MaxHeap([-num for num in initial])
 
-    def __len__(self) -> int:
-        return self.heap.__len__()
+    def get_min(self) -> Any:
+        return -self.heap.get_max()
 
+    def delete_min(self) -> Any:
+        self.heap.delete_max()
+
+    def extract_min(self) -> Any:
+        return -self.heap.extract_max()
+
+    def extract_min_and_insert(self, val: Any):
+        return -self.heap.extract_max_and_insert(-val)
+
+    def insert(self, val: Any):
+        self.heap.insert(-val)
+
+    # Knowledge of internal representaiton
     def decrease_key(self, i: int, decrement: int):
         self.heap.increase_key(i, -decrement)
 
-    def pop(self) -> Any:
-        return -self.heap.pop()
+    def delete(self, i: int) -> None:
+        self.decrease_key(i, -float("inf"))
+        self.delete_min()
 
-    def push(self, val: Any):
-        self.heap.push(-val)
-
-    def pushpop(self, val: Any):
-        return -self.heap.pushpop(-val)
-
-    def root(self) -> Any:
-        return -self.heap.root()
+    # Internal
+    def __len__(self) -> int:
+        return self.heap.__len__()
--- tests/data_structures/trees/test_heap.py
+++ tests/data_structures/trees/test_heap.py
@@ -4,33 +4,33 @@ from typing import List
 
 def test_max_heap():
     heap = MaxHeap()
-    heap.push(5)
-    heap.push(6)
-    heap.push(7)
-    assert (heap.pop() == 7)
-    assert (heap.root() == 6)
-    heap.push(9)
-    heap.push(13)
-    heap.push(11)
-    heap.push(10)
-    assert (heap.root() == 13)
+    heap.insert(5)
+    heap.insert(6)
+    heap.insert(7)
+    assert (heap.extract_max() == 7)
+    assert (heap.get_max() == 6)
+    heap.insert(9)
+    heap.insert(13)
+    heap.insert(11)
+    heap.insert(10)
+    assert (heap.get_max() == 13)
 
 
 def test_min_heap1():
     heap = MinHeap([4, 5, 8, 2])
-    assert (heap.pop() == 2)
+    assert (heap.extract_min() == 2)
 
 
 def test_min_heap2():
     heap = MinHeap([])
-    heap.push(-3)
-    assert (heap.root() == -3)
-    heap.push(-2)
-    heap.pop()
-    assert (heap.root() == -2)
-    heap.push(-4)
-    heap.pop()
-    assert (heap.root() == -2)
+    heap.insert(-3)
+    assert (heap.get_min() == -3)
+    heap.insert(-2)
+    heap.extract_min()
+    assert (heap.get_min() == -2)
+    heap.insert(-4)
+    heap.extract_min()
+    assert (heap.get_min() == -2)
 
 
 if __name__ == "__main__":
